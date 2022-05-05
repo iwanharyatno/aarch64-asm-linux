b31f0eaa09a755853e76630ceb8638d3f0c58d96
iwanharyatno <iwnharry61@gmail.com>
2022-05-06 05:29:05 +0700

    Menambahkan fungsi ubah string binary ke uint

--- utils.s
+++ utils.s
@@ -1,5 +1,5 @@
 .text
-    str2Udec:
+    str2UInt:
         stp     x29, x30, [sp, #-16]!
         stp     x4, x5, [sp, #-16]!
         stp     x2, x3, [sp, #-16]!
@@ -8,7 +8,7 @@
         mov     x5, #10
         mov     x4, #1
         mov     x0, #0
-    str2Udec_Count:
+    str2UInt_Count:
         sub     x2, x2, #1
         ldrb    w3, [x1, x2]
         sub     w3, w3, #48
@@ -17,7 +17,7 @@
         mul     x4, x4, x5
 
         cmp     x2, #0
-        bne     str2Udec_Count
+        bne     str2UInt_Count
 
         ldp     x6, x1, [sp], #16
         ldp     x2, x3, [sp], #16
@@ -47,5 +47,31 @@
 
         ldp     x0, x1, [sp], #16
         ldp     x2, x3, [sp], #16
+        ldp     x29, x30, [sp], #16
+        ret
+
+    strBin2UInt:
+        stp     x29, x30, [sp, #-16]!
+        stp     x5, x6, [sp, #-16]!
+        stp     x3, x4, [sp, #-16]!
+        stp     x1, x2, [sp, #-16]!
+
+        mov     x4, #2
+        mov     x3, #1
+        mov     x0, #0
+    strBin2UInt_Convert: 
+        sub     x2, x2, #1
+        ldrb    w5, [x1, x2]
+        sub     w5, w5, #48
+
+        madd    x0, x5, x3, x0
+        mul     x3, x3, x4
+
+        cmp     x2, #0
+        bne     strBin2UInt_Convert
+
+        ldp     x1, x2, [sp], #16
+        ldp     x3, x4, [sp], #16
+        ldp     x5, x6, [sp], #16
         ldp     x29, x30, [sp], #16
         ret
