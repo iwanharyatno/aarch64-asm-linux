49cd65aadfdf14b8ff0dae066e08d9dbb9aa342e
iwanharyatno <iwnharry61@gmail.com>
2022-05-07 08:48:04 +0700

    Mengupdate cara list menyimpan data
    Alih-alih menyimpan nilainya secara langsung dalam list, yang disimpan
    dalam list hanya panjang string dan pointer stringnya saja.

--- list.s
+++ list.s
@@ -1,34 +1,29 @@
 .text
+    // x0: pointer list
+    // x1: ukuran list
+    // x2: pointer string untuk dimasukkan ke dalam list
+    // x3: panjang string yang ditunjuk x2
     ladd:
 	    stp     x29, x30, [sp, #-16]!
         stp     x4, x5, [sp, #-16]!
         stp     x2, x3, [sp, #-16]!
 	    stp     x0, x1, [sp, #-16]!
-
-        mov     x5, #3
 	
-        ldrb    w4, [x0], #1
+        ldr     x4, [x0], #4
         cmp     x4, #0
-        beq     ladd_store_setlen
+        beq     ladd_store
     ladd_tail:
         subs    x1, x1, #1
         beq     ladd_exit
 
-        ldrb    w4, [x0], #1
+        ldr     x4, [x0], #4
 
         cmp     x4, #0
         bne     ladd_tail
-    ladd_store_setlen:
-        sub     x0, x0, #1
-        str     w3, [x0], #1
     ladd_store:
-        ldrb    w4, [x2], #1
-        strb    w4, [x0], #1
-        sub     x3, x3, #1
-
-        cmp     x3, #0
-        bne     ladd_store
-        strb    w5, [x0], #1
+        sub     x0, x0, #4
+        str     x3, [x0], #4
+        str     x2, [x0]
     ladd_exit:
 	    ldp     x0, x1, [sp], #16
         ldp     x2, x3, [sp], #16
@@ -44,16 +39,15 @@
         mov     x3, #0
         mov     x2, #0
     lprint_printElem:
-        ldrb    w2, [x0]
-        add     x0, x0, #1
-        mov     x1, x0
+        ldr     w2, [x0], #4
+        ldr     w1, [x0]
         bl      println
 
-        add     x0, x0, x2
+        add     x0, x0, #4
 
-        ldrb    w3, [x0], #1
-        cmp     w3, #3
-        beq     lprint_printElem
+        ldr     x3, [x0]
+        cmp     x3, #0
+        bne     lprint_printElem
         
         ldp     x0, x1, [sp], #16
         ldp     x2, x3, [sp], #16
@@ -62,23 +56,22 @@
 
     // x0: pointer list
     // x1: "indeks" elemen
+    // kembalian,
+    // x2: pointer hasil elemen
+    // x3: panjang dari string yang ditunjuk x2
     lget:
         stp     x29, x30, [sp, #-16]!
         stp     x0, x1, [sp, #-16]!
+        str     x4, [sp, #-16]! 
+        
+        mov     x4, #8
+        mul     x1, x1, x4
 
-        mov     x2, #0
-        mov     x3, #0
-    lget_find:
-        mov     x2, x0
-        ldrb    w3, [x0], #2
+        ldr     w3, [x0, x1]
+        add     x0, x0, x1
+        ldr     w2, [x0, #4]
 
-        add     x0, x0, x3
-
-        subs    x1, x1, #1
-        bne     lget_find
-
-        add     x2, x2, #1
-
+        ldr     x4, [sp], #16
         ldp     x0, x1, [sp], #16
         ldp     x29, x30, [sp], #16
         ret
